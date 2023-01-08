99d5eafea0bf98eced1984e4833de130240c051a
RamzesLys <ramzes311982@gmail.com>
2023-01-08 13:20:41 +0200

    add js gingerBread

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,1 +1,11 @@
-const slide = document.getElementsByClassName('slider_img');
+const gingerBread = document.getElementById('ginger-bread');
+gingerBread.style.rotate = -12 + 'deg';
+gingerBread.style.left = 5 + 'em';
+
+const sliderList = document.getElementById('slider-list');
+const [btnLeft, btnRight] = document.getElementsByClassName('slider_button');
+console.log('>>>', sliderList, btnLeft, btnRight);
+
+btnLeft.onclick = () => {
+    console.log ('Clicked')
+}
