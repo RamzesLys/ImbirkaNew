0e712e5f9cd3cd0aaa73d23560ee472f977d73e8
RamzesLys <ramzes311982@gmail.com>
2023-01-13 01:44:26 +0200

    Elipse Slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -16,14 +16,37 @@ const sliderList = document.getElementById('slider-list');
 const slides = document.querySelectorAll('.slide');
 const [btnLeft, btnRight] = document.getElementsByClassName('slider_button');
 
+let activeOrder = 0;
+
 const init = () => {
 	for (let i = 0; i < slides.length; i++) {
 		const slide = slides[i];
 		slide.dataset.order = i;
+		slide.style.transform = "translate(-50%, -50%)";
 	}
+	activeOrder = Math.floor(slides.length / 2)
 } 
 
 init()
+
+
+const update = () => {
+	const {width, height} = sliderList.getBoundingClientRect();
+
+	const a = Math.sqrt(width / 2);
+	const b = Math.sqrt(height / 2);
+
+	for (let i = 0; i < slides.length; i++) {
+		const leftSlide = document.querySelector('.slider[data-order="${activeOrder - i}"]'
+		);
+		if (leftSlide) {
+			leftSlide.style.left = `${width / 2 + }px`;
+		}
+		
+		const rightSlide = document.querySelector('.slider[data-order="${activeOrder + i}"]');
+
+	}
+}
 
 
 
