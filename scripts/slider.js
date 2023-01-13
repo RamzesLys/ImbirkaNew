624bf3bc657770382570c4a4c8ae3d2df9e3e0af
RamzesLys <ramzes311982@gmail.com>
2023-01-13 03:48:51 +0200

    Slider add (Vladislav)

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,55 +5,43 @@ const gingerBread = document.getElementById('ginger-bread');
 const position = () => {
 	setTimeout(() => {
 		gingerBread.classList.add('position')
-	}, 1000);
+	}, 500);
 }
 
 position();
 
 //SLIDER
-const sliderWrapper = document.querySelector('.slider_wrapper');
 const sliderList = document.getElementById('slider-list');
 const slides = document.querySelectorAll('.slide');
 const [btnLeft, btnRight] = document.getElementsByClassName('slider_button');
 
-let activeOrder = 0;
-
-const init = () => {
-	for (let i = 0; i < slides.length; i++) {
-		const slide = slides[i];
-		slide.dataset.order = i;
-		slide.style.transform = "translate(-50%, -50%)";
+const sliderContent = [
+	{
+		content:`слайд 1`
+	}, 
+	{
+		content: `слайд 2`
+	},
+	{
+		content: `слайд 3`
 	}
-	activeOrder = Math.floor(slides.length / 2)
-} 
+];
 
-init()
+sliderContent.forEach(item => {
+	const li = document.createElement('li');
+	li.textContent = item.content;
+	li.classList.add('slide')
+	sliderList.append(li);
 
 
-const update = () => {
-	const {width, height} = sliderList.getBoundingClientRect();
-
-	const a = Math.sqrt(width / 2);
-	const b = Math.sqrt(height / 2);
-
-	for (let i = 0; i < slides.length; i++) {
-		const leftSlide = document.querySelector('.slider[data-order="${activeOrder - i}"]'
-		);
-		if (leftSlide) {
-			leftSlide.style.left = `${width / 2 + }px`;
-		}
-		
-		const rightSlide = document.querySelector('.slider[data-order="${activeOrder + i}"]');
-
-	}
-}
+})
 
 
 
 btnLeft.onclick = () => {
-    console.log ('Clicked');
+	sliderList.style.left = 616 + 'px'
 }
 
 btnRight.onclick = () => {
-	console.log ('Clicked-right');
+	sliderList.style.left = sliderList.style.left - 616 + 'px'
 }
