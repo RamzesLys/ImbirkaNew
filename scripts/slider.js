963ab9bcc0aab893981dc8ac9f4c09e24eb281e2
RamzesLys <ramzes311982@gmail.com>
2023-01-21 01:00:30 +0200

    slider complete

--- scripts/slider.js
+++ scripts/slider.js
@@ -34,21 +34,32 @@ const sliderContent = [
 	} 
 ];
 
+let shift = 0;
+const width = -616;
+
 sliderContent.forEach(item => {
 	const li = document.createElement('li');
 	li.textContent = item.content;
 	li.classList.add('slide');
 	sliderList.append(li);
-
-
 })
 
-
-
 btnLeft.onclick = () => {
-	sliderList.style.left = 616 + 'px'
+	if (shift === 0) {
+		shift = width * (sliderContent.length - 1);
+	} else {
+		shift = shift - width;
+	}	
+	sliderList.style.left = shift + 'px';
 }
 
 btnRight.onclick = () => {
-	sliderList.style.left = sliderList.style.left - 616 + 'px'
-}
+	if (shift < width * (sliderContent.length - 2)) {
+		shift = 0;
+	} else {
+		shift = shift + width
+	}
+	sliderList.style.left = shift + 'px';
+	}
+	
+
